eb16230a4c16ef2292919899d8850fd12578a5d4
[kiereyes0812] <[reyeskierbrian]>
2025-10-22 14:26:35 +0800

    Added s87-89 code

--- index.js
+++ index.js
@@ -11,15 +11,11 @@ const postRoutes = require('./routes/post');
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 const corsOptions = {
-    origin: ['http://localhost:3000','http://127.0.0.1:3000', 'http://localhost:4000', 'http://localhost:5174', 'http://localhost:5173','http://localhost:5175'],
-    methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
-    allowedHeaders: ['Content-Type', 'Authorization'],
-    credentials: false,
+    origin: ['http://localhost:3000', 'https://in-class-prototyping-client.vercel.app'],
     optionsSuccessStatus: 204
 };
 
 app.use(cors(corsOptions));
-app.options('*', cors(corsOptions));
 
 mongoose.connect(process.env.MONGODB_STRING);
 mongoose.connection.once('open', () => console.log('Now connected to MongoDB Atlas.'));
