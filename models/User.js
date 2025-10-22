985aaea19b85cfeca0b309212261e823bbd18b4b
[kiereyes0812] <[reyeskierbrian]>
2025-10-22 14:03:55 +0800

    Troubleshooting

--- models/User.js
+++ models/User.js
@@ -5,17 +5,13 @@ const userSchema = new mongoose.Schema(
     firstName: { type: String, required: true, trim: true },
     lastName:  { type: String, required: true, trim: true },
     email:     { type: String, required: true, trim: true, lowercase: true, unique: true },
-    userName:  { type: String, required: true, trim: true, unique: true },
+    userName:  { type: String, required: true, trim: true, lowercase: true, unique: true },
     password:  { type: String, required: true },
     isAdmin:   { type: Boolean, default: false },
     mobileNo:  { type: String, required: true, trim: true }
   },
   { timestamps: true }
 );
-
-
-userSchema.index({ userName: 1 }, { unique: true, collation: { locale: "en", strength: 2 } });
-userSchema.index({ email: 1 }, { unique: true });
 
 
 userSchema.set("toJSON", {
